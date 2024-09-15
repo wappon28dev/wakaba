5322e618cf11811b25744a0920ca4fc2e057e69d
nasubi916 <r916nis@gmail.com>
2024-09-16 01:33:27 +0900

    💄 レイアウト完成

--- src/components/HorizontalScrolling.tsx
+++ src/components/HorizontalScrolling.tsx
@@ -3,12 +3,21 @@ import { type ReactElement } from "react";
 
 export function HorizontalScrolling({
   children,
+  title,
 }: {
   children: ReactElement;
+  title?: string;
 }): ReactElement {
   return (
-    <p.div overflowX="scroll" w="100%">
-      {children}
-    </p.div>
+    <>
+      {title !== "" && (
+        <p.p fontSize="xl" fontWeight="bold" ml={5} mt={2}>
+          {title}
+        </p.p>
+      )}
+      <p.div overflowX="scroll" w="100%">
+        {children}
+      </p.div>
+    </>
   );
 }
