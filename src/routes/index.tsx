378d17fd785c6b23bc5137675df29e1b5bdac97d
wappon28dev <wappon_28_dev@ruru.be>
2024-09-16 01:43:29 +0900

    Squashed commit of the following:
    commit e90cccd44f257ba09673edfe8d078688eb51379f
    Author: wappon28dev <wappon_28_dev@ruru.be>
    Date:   Mon Sep 16 01:42:04 2024 +0900
    
        🎨 マージ前微調整
    
    commit 82e932e6ee07411c27297c8757c5f7e1875a6d14
    Merge: 401ad8e e347f44
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 23:37:02 2024 +0900
    
        🔀 Merge remote-tracking branch 'origin/main' into root
    
    commit 401ad8e3c08e85c9de1b9e012d43729ccef96ff8
    Author: ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
    Date:   Sun Sep 15 11:32:21 2024 +0900
    
        📱 小さめの画面サイズへの配慮
    
    commit 4b2cd82073b9d2eda4f31b2832fecc4b1af89895
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 03:45:50 2024 +0900
    
        🚧 HorizontalScrolling追加
    
    commit 03630d0e7670a4e39a3e8ed61bd652e1da77a7a9
    Merge: 49f6c94 a7e7ae3
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 03:28:16 2024 +0900
    
        Merge remote-tracking branch 'origin/projects' into root
    
    commit a7e7ae3f4d5a268c0cb9696712fefe8cf7ab5409
    Author: nasubi916 <r916nis@gmail.com>
    Date:   Sun Sep 15 03:27:42 2024 +0900
    
        ✨ HorizontalScrollingの作成
    
    commit 49f6c94bcbfdeef6e96120928f372f12619c2cf6
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 03:09:24 2024 +0900
    
        🚧 プロジェクトカード追加
    
    commit 8ede535976b443bdb79ef66ab0b781b1ba1cb25b
    Merge: 72190c2 e651351
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 02:55:27 2024 +0900
    
        Merge remote-tracking branch 'origin/projects' into root
    
    commit 72190c28460d32c529a27260a12cd58f969de9e0
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 02:53:24 2024 +0900
    
        ♿️ スタイル変更
    
    commit e6513513a4e35726fbe8572debb2e6e91fd0ea73
    Author: nasubi916 <r916nis@gmail.com>
    Date:   Sun Sep 15 02:50:38 2024 +0900
    
        ✨ ProjectCardの作成
    
    commit 8f129c196018e1af84e8748c6213d62979b078c9
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 02:12:20 2024 +0900
    
        :construction: rootの作成
    
    commit ce1de51c02d8cf9d5ff09a1581c6ccbe618592aa
    Author: ROTO0504 <0504hiroosa@gmail.com>
    Date:   Sun Sep 15 00:06:50 2024 +0900
    
        ブランチ作成

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,188 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { styled as p } from "panda/jsx";
+import { HStack, styled as p } from "panda/jsx";
+import { HorizontalScrolling } from "@/components/HorizontalScrolling";
+import { LogoComposite } from "@/components/Logo";
+import { ProjectCard } from "@/components/project/Card";
 
 export const Route = createFileRoute("/")({
-  component: () => <p.p>Hello! </p.p>,
+  component: () => (
+    <p.div>
+      <p.div
+        alignItems="center"
+        bg="gray.200"
+        display="flex"
+        justifyContent="center"
+        py="20"
+        w="100%"
+      >
+        <LogoComposite
+          zoom={{
+            base: 2,
+            smDown: 1.5,
+          }}
+        />
+      </p.div>
+
+      <p.div
+        display="flex"
+        flexDirection="column"
+        height={{ base: "400px", sm: "500px" }}
+        justifyContent="center"
+      >
+        <p.div>
+          <p.h2 fontSize="200%" fontWeight="bold" textAlign="center">
+            Wakabaとは
+          </p.h2>
+          <p.p
+            fontSize="100%"
+            maxWidth={{ base: "85%", sm: "70%" }}
+            mt="20px"
+            mx="auto"
+            textAlign="center"
+          >
+            昔、あるところになかなか子どもが生まれない夫婦がいました。
+            でも、ある時、ようやくかわいらしい男の子が産まれました。
+            れどれ。あら、こぶなんてないじゃないの？」
+            「おばちゃん。名前が長すぎるから、もうこぶが引っ込んじゃったよ
+          </p.p>
+        </p.div>
+      </p.div>
+      <p.div>
+        <p.div pl="10%" textAlign="left">
+          <p.h3 fontSize="170%" fontWeight="bold">
+            現在募集中のプロジェクト
+          </p.h3>
+          <p.p>おすすめプロジェクト</p.p>
+          <HorizontalScrolling>
+            <HStack>
+              <ProjectCard
+                description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+                isFinished={false}
+                keyVisual="https://placehold.jp/250x150.png"
+                location="中区周辺"
+                name="タイトル"
+                tag="カフェテリア"
+                wateringPeople={1}
+              />
+              <ProjectCard
+                description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+                isFinished={false}
+                keyVisual="https://placehold.jp/250x150.png"
+                location="中区周辺"
+                name="タイトル"
+                tag="カフェテリア"
+                wateringPeople={1}
+              />
+              <ProjectCard
+                description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+                isFinished={false}
+                keyVisual="https://placehold.jp/250x150.png"
+                location="中区周辺"
+                name="タイトル"
+                tag="カフェテリア"
+                wateringPeople={1}
+              />
+            </HStack>
+          </HorizontalScrolling>
+          <HorizontalScrolling>
+            <HStack>
+              <ProjectCard
+                description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+                isFinished={false}
+                keyVisual="https://placehold.jp/250x150.png"
+                location="中区周辺"
+                name="タイトル"
+                tag="カフェテリア"
+                wateringPeople={1}
+              />
+              <ProjectCard
+                description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+                isFinished={false}
+                keyVisual="https://placehold.jp/250x150.png"
+                location="中区周辺"
+                name="タイトル"
+                tag="カフェテリア"
+                wateringPeople={1}
+              />
+              <ProjectCard
+                description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+                isFinished={false}
+                keyVisual="https://placehold.jp/250x150.png"
+                location="中区周辺"
+                name="タイトル"
+                tag="カフェテリア"
+                wateringPeople={1}
+              />
+            </HStack>
+          </HorizontalScrolling>
+        </p.div>
+      </p.div>
+      <p.div pl="10%" py={150} textAlign="left">
+        <p.h3 fontSize="170%" fontWeight="bold">
+          自分が蒔いた種
+        </p.h3>
+        <p.p>おすすめプロジェクト</p.p>
+        <HorizontalScrolling>
+          <HStack>
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区周辺"
+              name="タイトル"
+              tag="カフェテリア"
+              wateringPeople={1}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+          </HStack>
+        </HorizontalScrolling>
+      </p.div>
+    </p.div>
+  ),
 });
--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -1,6 +1,142 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { styled as p } from "panda/jsx";
+import { HStack } from "panda/jsx";
+import { HorizontalScrolling } from "@/components/HorizontalScrolling";
+import { ProjectCard } from "@/components/project/Card";
 
 export const Route = createFileRoute("/projects/")({
-  component: () => <p.p>Hello /projects/!</p.p>,
+  component: () => {
+    if (typeof window === "undefined") {
+      return null;
+    }
+    return (
+      <>
+        <HorizontalScrolling>
+          <HStack>
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区周辺"
+              name="タイトル"
+              tag="カフェテリア"
+              wateringPeople={1}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+          </HStack>
+        </HorizontalScrolling>
+        <HorizontalScrolling>
+          <HStack>
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区周辺"
+              name="タイトル"
+              tag="カフェテリア"
+              wateringPeople={1}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+          </HStack>
+        </HorizontalScrolling>
+        <HorizontalScrolling>
+          <HStack>
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区周辺"
+              name="タイトル"
+              tag="カフェテリア"
+              wateringPeople={1}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished={false}
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+            <ProjectCard
+              description="
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。
+          ここには生成AIが市民の意見を集約した文章が入ります。"
+              isFinished
+              keyVisual="https://placehold.jp/250x150.png"
+              location="中区一番街"
+              name="タイトル"
+              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
+              tag="カフェテリア"
+              wateringPeople={100}
+            />
+          </HStack>
+        </HorizontalScrolling>
+      </>
+    );
+  },
 });
