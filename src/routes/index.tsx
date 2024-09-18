8d43624d9415ad5b795181249edb058d2c410bed
ROTO0504 <0504hiroosa@gmail.com>
2024-09-18 21:50:22 +0900

    🚧 ルートページを更新

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { HStack, styled as p } from "panda/jsx";
-import { HorizontalScrolling } from "@/components/HorizontalScrolling";
 import { LogoComposite } from "@/components/Logo";
+import { Button } from "@/components/cva/Button";
 import { Expanded } from "@/components/cva/Expanded";
-import { ProjectCard } from "@/components/project/Card";
 
 export const Route = createFileRoute("/")({
   component: () => (
@@ -27,11 +26,16 @@ export const Route = createFileRoute("/")({
       <p.div
         display="flex"
         flexDirection="column"
-        height={{ base: "400px", sm: "500px" }}
+        height={{ base: "400px", sm: "700px" }}
         justifyContent="center"
       >
         <p.div>
-          <p.h2 fontSize="200%" fontWeight="bold" textAlign="center">
+          <p.h2
+            background="wkb.bg"
+            fontSize="300%"
+            fontWeight="bold"
+            textAlign="center"
+          >
             Wakabaとは
           </p.h2>
           <p.p
@@ -48,141 +52,147 @@ export const Route = createFileRoute("/")({
           </p.p>
         </p.div>
       </p.div>
-      <p.div>
-        <p.div pl="10%" textAlign="left">
-          <p.h3 fontSize="170%" fontWeight="bold">
-            現在募集中のプロジェクト
-          </p.h3>
-          <p.p>おすすめプロジェクト</p.p>
-          <HorizontalScrolling>
-            <HStack>
-              <ProjectCard
-                description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-                isFinished={false}
-                keyVisual="https://placehold.jp/250x150.png"
-                location="中区周辺"
-                name="タイトル"
-                tag="カフェテリア"
-                wateringPeople={1}
-              />
-              <ProjectCard
-                description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-                isFinished={false}
-                keyVisual="https://placehold.jp/250x150.png"
-                location="中区周辺"
-                name="タイトル"
-                tag="カフェテリア"
-                wateringPeople={1}
-              />
-              <ProjectCard
-                description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-                isFinished={false}
-                keyVisual="https://placehold.jp/250x150.png"
-                location="中区周辺"
-                name="タイトル"
-                tag="カフェテリア"
-                wateringPeople={1}
-              />
-            </HStack>
-          </HorizontalScrolling>
-          <HorizontalScrolling>
-            <HStack>
-              <ProjectCard
-                description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-                isFinished={false}
-                keyVisual="https://placehold.jp/250x150.png"
-                location="中区周辺"
-                name="タイトル"
-                tag="カフェテリア"
-                wateringPeople={1}
-              />
-              <ProjectCard
-                description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-                isFinished={false}
-                keyVisual="https://placehold.jp/250x150.png"
-                location="中区周辺"
-                name="タイトル"
-                tag="カフェテリア"
-                wateringPeople={1}
-              />
-              <ProjectCard
-                description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-                isFinished={false}
-                keyVisual="https://placehold.jp/250x150.png"
-                location="中区周辺"
-                name="タイトル"
-                tag="カフェテリア"
-                wateringPeople={1}
-              />
-            </HStack>
-          </HorizontalScrolling>
+
+      <p.div
+        background="wkb.primary"
+        display="flex"
+        flexDirection="column"
+        height={{ base: "500px", sm: "800px" }}
+        justifyContent="center"
+      >
+        <p.div>
+          <p.h2
+            color="wkb.bg"
+            fontSize="300%"
+            fontWeight="bold"
+            textAlign="center"
+          >
+            種を植える
+          </p.h2>
+          <p.p
+            color="wkb.bg"
+            fontSize="100%"
+            maxWidth={{ base: "85%", sm: "70%" }}
+            mt="20px"
+            mx="auto"
+            textAlign="center"
+          >
+            昔、あるところになかなか子どもが生まれない夫婦がいました。
+            でも、ある時、ようやくかわいらしい男の子が産まれました。
+            れどれ。あら、こぶなんてないじゃないの？」
+            「おばちゃん。名前が長すぎるから、もうこぶが引っ込んじゃったよ
+          </p.p>
         </p.div>
+        <Button
+          _hover={{
+            background: "wkb.bg",
+            shadow: "md",
+          }}
+          background="wkb.bg"
+          fontSize="150%"
+          fontWeight="bold"
+          mt="50px"
+          mx="auto"
+          p="10px 20px"
+        >
+          実際に初めてみる
+        </Button>
       </p.div>
-      <p.div pl="10%" py={150} textAlign="left">
-        <p.h3 fontSize="170%" fontWeight="bold">
-          自分が蒔いた種
-        </p.h3>
-        <p.p>おすすめプロジェクト</p.p>
-        <HorizontalScrolling>
-          <HStack>
-            <ProjectCard
-              description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-              isFinished={false}
-              keyVisual="https://placehold.jp/250x150.png"
-              location="中区周辺"
-              name="タイトル"
-              tag="カフェテリア"
-              wateringPeople={1}
-            />
-            <ProjectCard
-              description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-              isFinished={false}
-              keyVisual="https://placehold.jp/250x150.png"
-              location="中区一番街"
-              name="タイトル"
-              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
-              tag="カフェテリア"
-              wateringPeople={100}
-            />
-            <ProjectCard
-              description="
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。
-          ここには生成AIが市民の意見を集約した文章が入ります。"
-              isFinished
-              keyVisual="https://placehold.jp/250x150.png"
-              location="中区一番街"
-              name="タイトル"
-              sponsorIcon="https://avatars.githubusercontent.com/u/104073343?v=4"
-              tag="カフェテリア"
-              wateringPeople={100}
-            />
-          </HStack>
-        </HorizontalScrolling>
+      <p.div
+        background="wkb.bg"
+        display="flex"
+        flexDirection="column"
+        height={{ base: "500px", sm: "800px" }}
+        justifyContent="center"
+      >
+        <p.div>
+          <p.h2
+            color="wkb.text"
+            fontSize="300%"
+            fontWeight="bold"
+            textAlign="center"
+          >
+            Projects
+          </p.h2>
+          <p.p
+            color="wkb.text"
+            fontSize="100%"
+            maxWidth={{ base: "85%", sm: "70%" }}
+            mt="20px"
+            mx="auto"
+            textAlign="center"
+          >
+            試しに好きなジャンルのプロジェクトを調べてみる
+          </p.p>
+        </p.div>
+        <HStack pt={150} px="5%">
+          <p.a
+            height={100}
+            href="/projects?q=cafe"
+            mt="50px"
+            mx="auto"
+            width={{ base: "200", md: "300" }}
+          >
+            <Button
+              _hover={{
+                background: "wkb.primary",
+                shadow: "md",
+              }}
+              background="wkb.primary"
+              color="wkb.bg"
+              fontSize="150%"
+              fontWeight="bold"
+              height="100%"
+              width="100%"
+            >
+              # カフェ
+            </Button>
+          </p.a>
+          <p.a
+            fontWeight="bold"
+            height={100}
+            href="/projects?q=cafe"
+            mt="50px"
+            mx="auto"
+            width={{ base: "200", md: "300" }}
+          >
+            <Button
+              _hover={{
+                background: "wkb.primary",
+                shadow: "md",
+              }}
+              background="wkb.primary"
+              color="wkb.bg"
+              fontSize="150%"
+              height="100%"
+              width="100%"
+            >
+              # 図書館
+            </Button>
+          </p.a>{" "}
+          <p.a
+            height={100}
+            href="/projects?q=gym"
+            mt="50px"
+            mx="auto"
+            width={{ base: "200", md: "300" }}
+          >
+            <Button
+              _hover={{
+                background: "wkb.primary",
+                shadow: "md",
+              }}
+              background="wkb.primary"
+              color="wkb.bg"
+              fontSize="150%"
+              fontWeight="bold"
+              height="100%"
+              width="100%"
+            >
+              # 運動施設
+            </Button>
+          </p.a>
+        </HStack>
       </p.div>
     </Expanded>
   ),
