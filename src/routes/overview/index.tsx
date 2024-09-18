8307a2f20d8aec92233190ca6cb638d6b337f17c
nasubi916 <r916nis@gmail.com>
2024-09-19 02:08:37 +0900

    💄 projectの位置を表示

--- src/routes/overview/index.tsx
+++ src/routes/overview/index.tsx
@@ -2,6 +2,27 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Box, styled as p } from "panda/jsx";
 import { Map } from "./-components/Map";
 
+type projects = {
+  amount_of_money: number;
+  comment_ids: string[];
+  created_at: string;
+  deadline: string;
+  key_visual: string | null;
+  name: string;
+  project_id: string;
+  sponsor_data_id: string | null;
+  territory_id: string;
+  fruit_ids: string[];
+  location: {
+    lat: number;
+    lng: number;
+  };
+  motivation: string;
+  report_ids: string[];
+  sponsor_id: string;
+  target_amount_of_money: number;
+};
+
 export const Route = createFileRoute("/overview/")({
   component: () => {
     const currentUserLocation = {
@@ -9,10 +30,47 @@ export const Route = createFileRoute("/overview/")({
       lng: 139.6917,
     };
 
+    const projects: projects[] = [
+      {
+        amount_of_money: 100,
+        comment_ids: ["1"],
+        created_at: "2021-09-06T00:00:00Z",
+        deadline: "2021-09-06T00:00:00Z",
+        key_visual: "https://via.placeholder.com/150",
+        name: "Project 1",
+        project_id: "1",
+        sponsor_data_id: "1",
+        territory_id: "1",
+        fruit_ids: ["1"],
+        location: { lat: 35.688, lng: 139.69 },
+        motivation: "motivation",
+        report_ids: ["1"],
+        sponsor_id: "1",
+        target_amount_of_money: 1000,
+      },
+      {
+        amount_of_money: 200,
+        comment_ids: ["2"],
+        created_at: "2021-09-06T00:00:00Z",
+        deadline: "2021-09-06T00:00:00Z",
+        key_visual: "https://via.placeholder.com/150",
+        name: "Project 2",
+        project_id: "2",
+        sponsor_data_id: "2",
+        territory_id: "2",
+        fruit_ids: ["2"],
+        location: { lat: 35.6895, lng: 139.69397 },
+        motivation: "motivation",
+        report_ids: ["2"],
+        sponsor_id: "2",
+        target_amount_of_money: 2000,
+      },
+    ];
+
     return (
       <p.div>
         <Box maxH="100dvh" width="100%">
-          <Map currentUserLocation={currentUserLocation} />
+          <Map currentUserLocation={currentUserLocation} projects={projects} />
         </Box>
       </p.div>
     );
