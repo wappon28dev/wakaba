78929d67845630990c9fa925e22d37e33ef65f9d
ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
2024-09-19 20:52:58 +0900

    🚧 seedsある程度完成

--- src/routes/_auth/seeds/index.tsx
+++ src/routes/_auth/seeds/index.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { styled as p } from "panda/jsx";
+import { Grid, styled as p } from "panda/jsx";
 import { useInView } from "react-intersection-observer";
 import { SownSeed } from "./-components/sownSeed";
+import { GridLayout } from "@/components/GridLayout";
 import { Button } from "@/components/cva/Button";
 import { Expanded } from "@/components/cva/Expanded";
+import { ProjectCard } from "@/components/project/Card";
 
 export const Route = createFileRoute("/_auth/seeds/")({
   component: () => {
@@ -14,11 +16,6 @@ export const Route = createFileRoute("/_auth/seeds/")({
 
     return (
       <Expanded alignItems="start">
-        <SownSeed
-          category="花"
-          createdAt="2021.10.10"
-          description="これはテストです。"
-        />
         <p.div
           ref={ref1}
           background="wkb.bg"
@@ -59,14 +56,114 @@ export const Route = createFileRoute("/_auth/seeds/")({
           <p.h2 fontSize="1rem" fontWeight="bold" my={10} textAlign="left">
             自分が蒔いた種
           </p.h2>
-          <p.div>
-            <p.p height={500}>ここには自分が蒔いた種が表示されます。</p.p>
-          </p.div>{" "}
-          <p.h2 fontSize="1rem" fontWeight="bold" my={10} textAlign="left">
-            目が出た種
-          </p.h2>
-          <p.div>
-            <p.p height={500}>ここには自分が蒔いた種が表示されます。</p.p>
+          <p.div display="flex" flexWrap="nowrap" gap={4} overflowX="auto">
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+          </p.div>
+          <p.div display="flex" flexWrap="nowrap" gap={4} overflowX="auto">
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+            <SownSeed
+              category="花"
+              createdAt="2021.10.10"
+              description="これはテストです。"
+            />
+          </p.div>
+          <p.div my={50}>
+            <p.h2 fontSize="1rem" fontWeight="bold" my={10} textAlign="left">
+              目が出た種
+            </p.h2>
+            <GridLayout>
+              <>
+                <ProjectCard
+                  amountOfMoney={100000}
+                  keyVisual="https://placehold.jp/300x150.png"
+                  location="中区周辺"
+                  name="タイトル"
+                />
+                <ProjectCard
+                  amountOfMoney={100000}
+                  keyVisual="https://placehold.jp/300x150.png"
+                  location="中区一番街"
+                  name="タイトル"
+                />
+                <ProjectCard
+                  amountOfMoney={100000}
+                  keyVisual="https://placehold.jp/300x150.png"
+                  location="中区一番街"
+                  name="タイトル"
+                />
+                <ProjectCard
+                  amountOfMoney={100000}
+                  keyVisual="https://placehold.jp/300x150.png"
+                  location="中区一番街"
+                  name="タイトル"
+                />
+                <ProjectCard
+                  amountOfMoney={100000}
+                  keyVisual="https://placehold.jp/300x150.png"
+                  location="中区一番街"
+                  name="タイトル"
+                />
+                <ProjectCard
+                  amountOfMoney={100000}
+                  keyVisual="https://placehold.jp/300x150.png"
+                  location="中区一番街"
+                  name="タイトル"
+                />
+              </>
+            </GridLayout>
           </p.div>
         </p.div>
       </Expanded>
