36f6fc7a99459f76420545a53ffe61ea0aff6771
ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
2024-09-20 01:01:23 +0900

    🚧 緯度経度取得

--- src/routes/_auth/seeds/new/index.tsx
+++ src/routes/_auth/seeds/new/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { styled as p } from "panda/jsx";
 import { useEffect, useState } from "react";
+import { Button } from "@/components/cva/Button";
+import { yahooRevGeoAPI } from "@/lib/services/address";
 
 export const Route = createFileRoute("/_auth/seeds/new/")({
   component: () => {
@@ -8,24 +10,20 @@ export const Route = createFileRoute("/_auth/seeds/new/")({
       lat: number;
       lon: number;
     } | null>(null);
-    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
           setLocation({
             lat: position.coords.latitude,
             lon: position.coords.longitude,
           });
-        },
-        () => {
-          setError("位置情報を取得できませんでした。");
-        },
-      );
-    }, []);
+        });
+      }
+    });
 
     return (
-      <p.p>
+      <p.div>
         {location != null ? (
           <div>
             <p.p>現在の位置情報:</p.p>
@@ -33,9 +31,18 @@ export const Route = createFileRoute("/_auth/seeds/new/")({
             <p.p>経度: {location.lon}</p.p>
           </div>
         ) : (
-          <p.p>{error ?? "位置情報を取得中..."}</p.p>
+          <p.p>位置情報を取得中...</p.p>
         )}
-      </p.p>
+        <Button
+          onClick={() => {
+            (Promise.resolve(yahooRevGeoAPI(1, 1)) as Promise<unknown>)
+              .then(() => {})
+              .catch(() => {});
+          }}
+        >
+          a
+        </Button>
+      </p.div>
     );
   },
 });
