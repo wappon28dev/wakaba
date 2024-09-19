4b15c54fef97ebed2cd0976a55660babca633fc6
nasubi916 <r916nis@gmail.com>
2024-09-20 00:43:36 +0900

    project 完成

--- src/routes/_auth/projects/index.tsx
+++ src/routes/_auth/projects/index.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from "@ark-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
-import { HStack, styled as p } from "panda/jsx";
-import { Pagination, Navigation } from "swiper/modules";
+import { Flex, HStack, styled as p } from "panda/jsx";
+import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { GridLayout } from "@/components/GridLayout";
 import { ProjectCard } from "@/components/project/Card";
@@ -18,7 +18,6 @@ type Project = {
   amountOfMoney: number;
   status: "wakaba" | "seed" | "tree";
   keyVisual: string;
-  
 };
 
 export const Route = createFileRoute("/_auth/projects/")({
@@ -55,11 +54,8 @@ export const Route = createFileRoute("/_auth/projects/")({
             }}
             centeredSlides
             loop
-            modules={[Pagination, Navigation]}
+            modules={[Navigation]}
             navigation
-            pagination={{
-              clickable: true,
-            }}
           >
             <HStack>
               {[...Array(9)].map((_) => (
@@ -77,70 +73,53 @@ export const Route = createFileRoute("/_auth/projects/")({
           </Swiper>
         </p.div>
 
-        <p.div
-          alignItems="center"
-          bg="wkb-neutral.0"
-          display="flex"
-          h={300}
-          justifyContent="center"
-          position="relative"
-          rounded="md"
-          w="100%"
-        >
-          <p.h1
-            color="wkb-neutral.800"
-            fontSize="4xl"
-            fontWeight="bold"
-            pos="absolute"
-            textAlign="center"
-          >
-            Projects
-          </p.h1>
-        </p.div>
-
-        <Tabs.Root className={tabs.root} defaultValue="recommend">
-          <Tabs.List className={tabs.list}>
-            <Tabs.Trigger className={tabs.trigger} value="recommend">
-              おすすめ
-            </Tabs.Trigger>
-            <Tabs.Trigger className={tabs.trigger} value="trending">
-              急上昇
-            </Tabs.Trigger>
-            <Tabs.Trigger className={tabs.trigger} value="all">
-              すべて
-            </Tabs.Trigger>
-          </Tabs.List>
-          <Tabs.Content value="recommend">
-            <GridLayout>
-              <>
-                {/* //!いずれ mapでループさせる */}
-                <ProjectCard
-                  amountOfMoney={100000}
-                  keyVisual="https://placehold.jp/300x150.png"
-                  location="中区周辺"
-                  name="タイトル"
-                  status="wakaba"
-                />
-                <ProjectCard
-                  amountOfMoney={100000}
-                  keyVisual="https://placehold.jp/300x150.png"
-                  location="中区一番街"
-                  name="タイトル"
-                  status="tree"
-                />
-                <ProjectCard
-                  amountOfMoney={100000}
-                  keyVisual="https://placehold.jp/300x150.png"
-                  location="中区一番街"
-                  name="タイトル"
-                  status="seed"
-                />
-              </>
-            </GridLayout>
-          </Tabs.Content>
-          <Tabs.Content value="trending">あとで</Tabs.Content>
-          <Tabs.Content value="all">つくるね</Tabs.Content>
-        </Tabs.Root>
+        <Flex justify="center">
+          <p.div maxW="1200px" w="100%">
+            <Tabs.Root className={tabs.root} defaultValue="recommend">
+              <Tabs.List className={tabs.list}>
+                <Tabs.Trigger className={tabs.trigger} value="recommend">
+                  おすすめ
+                </Tabs.Trigger>
+                <Tabs.Trigger className={tabs.trigger} value="trending">
+                  急上昇
+                </Tabs.Trigger>
+                <Tabs.Trigger className={tabs.trigger} value="all">
+                  すべて
+                </Tabs.Trigger>
+              </Tabs.List>
+              <Tabs.Content value="recommend">
+                <GridLayout>
+                  <>
+                    {/* //!いずれ mapでループさせる */}
+                    <ProjectCard
+                      amountOfMoney={100000}
+                      keyVisual="https://placehold.jp/300x150.png"
+                      location="中区周辺"
+                      name="タイトル"
+                      status="wakaba"
+                    />
+                    <ProjectCard
+                      amountOfMoney={100000}
+                      keyVisual="https://placehold.jp/300x150.png"
+                      location="中区一番街"
+                      name="タイトル"
+                      status="tree"
+                    />
+                    <ProjectCard
+                      amountOfMoney={100000}
+                      keyVisual="https://placehold.jp/300x150.png"
+                      location="中区一番街"
+                      name="タイトル"
+                      status="seed"
+                    />
+                  </>
+                </GridLayout>
+              </Tabs.Content>
+              <Tabs.Content value="trending">あとで</Tabs.Content>
+              <Tabs.Content value="all">つくるね</Tabs.Content>
+            </Tabs.Root>
+          </p.div>
+        </Flex>
       </>
     );
   },
