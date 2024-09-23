0d91e3e7c2609293e6f0af46797e5c0a0913180c
wappon28dev <wappon_28_dev@ruru.be>
2024-09-24 00:35:11 +0900

    ✨ `/demo` で画像生成機能

--- src/components/project/Card.tsx
+++ src/components/project/Card.tsx
@@ -1,14 +1,13 @@
 import { Icon } from "@iconify/react";
-import { styled as p, HStack } from "panda/jsx";
+import { styled as p, HStack, VStack } from "panda/jsx";
 
 import { type ReactElement } from "react";
 import useSWRImmutable from "swr/immutable";
 import { match } from "ts-pattern";
 import { ICON } from "@/assets/icon";
-import { Loading } from "@/components/Loading";
-import { Expanded } from "@/components/cva/Expanded";
 import { fetchAddressFromLocation } from "@/lib/services/address";
 import { S } from "@/lib/utils/patterns";
+import { notifyErrorInToast } from "@/lib/utils/toast";
 
 export function ProjectCard({
   name,
@@ -37,63 +36,79 @@ export function ProjectCard({
     )._unsafeUnwrap(),
   );
 
-  return match(swrLocation)
-    .with(S.Loading, () => (
-      <Expanded basedOn="screen" items="center">
-        <Loading>
-          <p.p>わかばの起動中...</p.p>
-        </Loading>
-      </Expanded>
-    ))
-    .with(S.Success, ({ data: { Feature } }) => (
-      <p.div
-        bg="wkb-neutral.0"
-        fontSize="sm"
-        m={4}
-        mdDown={{ minW: "90%" }}
-        minH={300}
-        minW={300}
-        p={4}
-        rounded="md"
-        w="20%"
-      >
-        <p.div position="relative">
-          <p.img
-            alt="Placeholder"
-            h="1/2"
-            objectFit="cover"
-            rounded="md"
-            src={key_visual}
-            w="100%"
+  return (
+    <VStack
+      alignItems="start"
+      bg="wkb-neutral.0"
+      fontSize="sm"
+      m={4}
+      mdDown={{ minW: "90%" }}
+      minH={300}
+      minW={300}
+      p={4}
+      rounded="md"
+      w="20%"
+    >
+      <p.div position="relative">
+        <p.img
+          alt="Placeholder"
+          h="1/2"
+          objectFit="cover"
+          rounded="md"
+          src={key_visual}
+          w="100%"
+        />
+      </p.div>
+      <HStack gap="1">
+        {match(swrLocation)
+          .with(S.Loading, () => (
+            <>
+              <Icon icon="svg-spinners:ring-resize" />
+              <p.p>住所を取得中...</p.p>
+            </>
+          ))
+          .with(S.Success, ({ data }) => {
+            const addr = data.Feature.at(0)?.Property.AddressElement;
+            const addrStr = match(status)
+              .with("wakaba", () => `${addr?.at(2)?.Name}周辺`)
+              .otherwise(() => addr?.at(2)?.Name ?? addr?.at(3)?.Name ?? "");
+            return (
+              <>
+                <Icon icon="bi:geo-alt-fill" />
+                <p.p>{addrStr}</p.p>
+              </>
+            );
+          })
+          .with(S.Error, ({ error }) => {
+            notifyErrorInToast(
+              "swrLocation",
+              "住所の取得中にエラーが発生しました",
+              new Error(error as string),
+            );
+            return (
+              <p.p color="wkb.secondary">
+                住所の取得中にエラーが発生しました
+              </p.p>
+            );
+          })
+          .otherwise(() => null)}
+      </HStack>
+      <p.p flex="1" fontSize="2xl" minH="3em">
+        {name}
+      </p.p>
+      <p.p fontSize="md">現在金額 ￥{amount_of_money}</p.p>
+      <HStack justifyContent="space-between" w="100%">
+        <HStack>
+          <Icon height="1.5em" icon="mdi:star-outline" />
+          <Icon height="1.5em" icon="mdi:share-variant" />
+        </HStack>
+        <p.div alignItems="baseline" ml="auto">
+          <Icon
+            height={status === "hana" ? "2rem" : "1.5rem"}
+            icon={ICON[status]}
           />
         </p.div>
-        <p.div h="1/2" pt={4}>
-          <p.span>
-            <HStack gap="-1">
-              <Icon icon="bi:geo-alt-fill" />
-              {status === "wakaba"
-                ? `${Feature.at(0)?.Property.AddressElement[2]?.Name}周辺`
-                : Feature.at(0)?.Property.AddressElement[2]?.Name ??
-                  `${Feature.at(0)?.Property.AddressElement[3]?.Name}` ??
-                  ""}
-            </HStack>
-          </p.span>
-          <p.p fontSize="2xl" fontWeight="bold">
-            {name}
-          </p.p>
-          <p.p fontSize="md">現在金額 ￥{amount_of_money}</p.p>
-          <HStack>
-            <Icon icon="mdi:star-outline" width={30} />
-            <Icon icon="mdi:share-variant" width={30} />
-            <p.div alignItems="baseline" ml="auto">
-              <Icon
-                height={status === "hana" ? "2rem" : "1.5rem"}
-                icon={ICON[status]}
-              />
-            </p.div>
-          </HStack>
-        </p.div>
-      </p.div>
-    ))
-    .otherwise(({ error }) => <p.p>{error.Error.Message}</p.p>);
+      </HStack>
+    </VStack>
+  );
 }
--- src/routes/demo/index.tsx
+++ src/routes/demo/index.tsx
@@ -1,51 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { css } from "panda/css";
-import { VStack, styled as p } from "panda/jsx"; // NOTE: この行のスニペット: `pd` → Tab
-// import { PopoverSample } from "./-components/CheckBox";
-// import { GeminiDemo } from "./-components/GeminiTest";
-import { useEffect } from "react";
-import { GeminiDemo2 } from "./-components/GeminiTest2";
-import { fetchAddressFromLocation } from "@/lib/services/address";
-// import { Logo, LogoComposite } from "@/components/Logo";
+import { VStack, styled as p } from "panda/jsx";
+import { ProjectCreation } from "./-components/ProjectCreation";
 
 export const Route = createFileRoute("/demo/")({
-  component: () => {
-    useEffect(() => {
-      void fetchAddressFromLocation({ lat: 35.681236, lon: 139.767125 }).then(
-        (res) => {
-          // eslint-disable-next-line no-console
-          console.log(res);
-        },
-      );
-    }, []);
-
-    return (
-      <VStack
-        alignItems="start"
-        className={css({ "& > h2": { fontWeight: "bold" } })}
-        p="2"
-      >
-        {/* <p.h2>
-        Ark UI + Atomic Slot Recipe (<p.code>sva</p.code>) demo
-      </p.h2>
-      <PopoverSample />
-      <p.h2>Logo</p.h2>
-      <HStack>
-        <p.p>シンボルロゴ</p.p>
-        <Logo variant="sym" />
-      </HStack>
-      <HStack>
-        <p.p>タイトルロゴ</p.p>
-        <Logo variant="title" />
-      </HStack>
-      <HStack>
-        <p.p>複合 (4:1) ロゴ</p.p>
-        <LogoComposite />
-      </HStack> */}
-        <p.h2>Gemini API Demo</p.h2>
-        {/* <GeminiDemo /> */}
-        <GeminiDemo2 />
-      </VStack>
-    );
-  },
+  component: () => (
+    <VStack
+      alignItems="start"
+      className={css({ "& > h2": { fontWeight: "bold" } })}
+      p="2"
+    >
+      <p.h2>Gemini API Demo</p.h2>
+      <ProjectCreation />
+    </VStack>
+  ),
 });
