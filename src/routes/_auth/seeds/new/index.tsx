2ad6c08c895ca0ae1e6e2f2a1f45435aa762f8db
ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
2024-09-23 12:56:11 +0900

    🚧 react-hook-form との連携

--- src/routes/_auth/seeds/new/index.tsx
+++ src/routes/_auth/seeds/new/index.tsx
@@ -6,10 +6,11 @@ import { useForm } from "react-hook-form";
 import { Button } from "@/components/cva/Button";
 import { svaField } from "@/components/sva/field";
 import { fetchAddressFromLocation } from "@/lib/services/address";
+import { toaster } from "@/lib/utils/toast";
 
 type IFormInput = {
-  location: unknown;
-  category: string;
+  location: string;
+  category_id: "休憩" | "環境" | "飲食" | "施設" | "移動" | "その他";
   description: string;
 };
 
@@ -21,58 +22,60 @@ type Param = {
 const field = svaField();
 export const Route = createFileRoute("/_auth/seeds/new/")({
   component: () => {
-    const { register } = useForm<IFormInput>();
+    const { register, handleSubmit, setValue, reset } = useForm<IFormInput>();
     const [location, setLocation] = useState<{
       lat: number;
       lon: number;
     } | null>(null);
-    const [address, setAddress] = useState<string | null>(null);
+    const [address, setAddress] = useState<string>();
 
     useEffect(() => {
       if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setLocation({
-              lat: position.coords.latitude,
-              lon: position.coords.longitude,
-            });
-          },
-          (error) => {
-            console.error("位置情報の取得に失敗しました:", error);
-          },
-        );
+        navigator.geolocation.getCurrentPosition((position) => {
+          const newLocation = {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          };
+          setLocation(location);
+          void (async () => {
+            try {
+              const param: Param = {
+                lat: newLocation.lat,
+                lon: newLocation.lon,
+              };
+
+              const response = await fetchAddressFromLocation(param);
+              if (response.isOk()) {
+                const resAddress =
+                  response.value?.Feature?.[0]?.Property?.Address;
+                setAddress(resAddress);
+                if (resAddress !== undefined) {
+                  setValue("location", resAddress);
+                }
+              }
+            } catch (error) {}
+          })();
+        });
       }
     }, []);
-
-    const handleButtonClick = async (): Promise<void> => {
-      try {
-        if (location != null) {
-          const param: Param = {
-            lat: location.lat,
-            lon: location.lon,
-          };
-
-          console.log("位置情報:", param);
-
-          const response = await fetchAddressFromLocation(param);
-          setAddress(address);
-          if (response.isOk()) {
-            const resAddress = response.value?.Feature?.[0]?.Property?.Address;
-            console.log("住所:", resAddress);
-          } else {
-            console.error("APIエラー:", response.error);
-          }
-        } else {
-          console.error("位置情報が設定されていません。");
-        }
-      } catch (error) {
-        console.error("エラーが発生しました:", error);
+    const onSubmit = (data: IFormInput): void => {
+      // 入力チェック
+      if (data.category_id.length === 0 || data.description.length === 0) {
+        toaster.error({
+          id: "form-error",
+          title: "エラー",
+          description: "カテゴリーと意見は必須です",
+          duration: 5000,
+        });
+        return;
       }
-    };
 
-    const handleButtonClickWrapper = (): void => {
-      handleButtonClick().catch((error) => {
-        console.error("エラーが発生しました:", error);
+      console.log("フォームデータ:", data);
+      reset();
+      toaster.success({
+        id: "login-success",
+        title: "新しい種を植えました",
+        duration: 5000,
       });
     };
 
@@ -90,67 +93,64 @@ export const Route = createFileRoute("/_auth/seeds/new/")({
             </p.h2>
             <p.p textAlign="center">新しい種を植える</p.p>
           </p.div>
-          <Field.Root className={field.root}>
-            <p.div p={2}>
-              <p.p fontWeight="bold">位置情報</p.p>
-              <Field.Input
-                {...register("location")}
-                className={field.input}
-                disabled
-                inputMode="text"
-                placeholder={
-                  location != null
-                    ? `経度：${location.lat},緯度：${location.lon}`
-                    : "取得中..."
-                }
-              />
-            </p.div>
-            <p.div p={2}>
-              <p.p fontWeight="bold" py={2}>
-                カテゴリー
-              </p.p>
-              <Field.Select {...register("category")} className={field.select}>
-                <option value="">選択してください</option>
-                <option value="">休憩</option>
-                <option value="2">環境</option>
-                <option value="3">飲食</option>
-                <option value="4">施設</option>
-                <option value="5">移動</option>
-                <option value="6">その他</option>
-              </Field.Select>
-            </p.div>
-            <p.div p={2}>
-              <p.p fontWeight="bold" py={2}>
-                意見
-              </p.p>
-              <Field.Textarea
-                {...register("description")}
-                className={field.textarea}
-                inputMode="text"
-                placeholder="意見を入力してください"
-              />
-            </p.div>
-            <Button
-              _hover={{
-                background: "wkb.primary",
-              }}
-              background="wkb.primary"
-              color="wkb.bg"
-              fontSize="1rem"
-              fontWeight="bold"
-              onClick={handleButtonClickWrapper}
-              rounded="md"
-              type="button"
-            >
-              種を蒔く
-            </Button>
-            {address != null && (
-              <p.div mt={4}>
-                <p.p fontWeight="bold">取得した住所:</p.p>
-                <p.p>{address}</p.p>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              void handleSubmit(onSubmit)(e);
+            }}
+          >
+            <Field.Root className={field.root}>
+              <p.div p={2}>
+                <p.p fontWeight="bold">位置情報</p.p>
+                <Field.Input
+                  {...register("location")}
+                  className={field.input}
+                  disabled
+                  inputMode="text"
+                  placeholder={address != null ? `${address}` : "取得中..."}
+                />
               </p.div>
-            )}
-          </Field.Root>
+              <p.div p={2}>
+                <p.p fontWeight="bold" py={2}>
+                  カテゴリー
+                </p.p>
+                <Field.Select
+                  {...register("category_id")}
+                  className={field.select}
+                >
+                  <option value="">選択してください</option>
+                  <option value="休憩">休憩</option>
+                  <option value="環境">環境</option>
+                  <option value="飲食">飲食</option>
+                  <option value="施設">施設</option>
+                  <option value="移動">移動</option>
+                  <option value="その他">その他</option>
+                </Field.Select>
+              </p.div>
+              <p.div p={2}>
+                <p.p fontWeight="bold" py={2}>
+                  意見
+                </p.p>
+                <Field.Textarea
+                  {...register("description")}
+                  className={field.textarea}
+                  inputMode="text"
+                  placeholder="意見を入力してください"
+                />
+              </p.div>
+              <Button
+                background="wkb.primary"
+                color="wkb.bg"
+                disabled={address == null}
+                fontSize="1rem"
+                fontWeight="bold"
+                rounded="md"
+                type="submit"
+              >
+                種を蒔く
+              </Button>
+            </Field.Root>
+          </form>
         </p.div>
       </p.div>
     );
