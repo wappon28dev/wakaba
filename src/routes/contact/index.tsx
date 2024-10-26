48ed9081626192940f98eee28a0c708ef993d308
ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
2024-10-27 00:32:17 +0900

    first commit

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,5 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { styled as p } from "panda/jsx";
 
 export const Route = createFileRoute("/contact/")({
-  component: () => <div>Hello /contact/!</div>,
+  component: () => {
+    const { uuid } = Route.useParams<{ uuid: string }>();
+
+    return (
+      <p.div>
+        <p.h1>Contact</p.h1>
+        <p.p>Hello /contact/! - {uuid}</p.p>
+      </p.div>
+    );
+  },
 });
