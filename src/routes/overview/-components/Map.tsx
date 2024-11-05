c374ed34f0d55bdd74a903574ba924df2770984c
nasubi916 <r916nis@gmail.com>
2024-11-05 22:24:12 +0900

    ピンサイズ修正

--- src/routes/overview/-components/Map.tsx
+++ src/routes/overview/-components/Map.tsx
@@ -52,7 +52,7 @@ function MarkerRenderer({
               iconUrl: `./src/assets/img/${project.calcStatus(referenced)}.png`,
               shadowUrl:
                 "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-              iconSize: [25, 41],
+              iconSize: [41, 41],
               iconAnchor: [12, 41],
               popupAnchor: [1, -34],
               shadowSize: [41, 41],
