6106ebfb312c8f34bbbfab73ce69293912c84c0a
ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
2024-11-07 12:08:24 +0900

    Squashed commit of the following:
    commit 4f7d49dc1d22fa43f610ef8e79840a13c9fc87e0
    Author: ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
    Date:   Thu Nov 7 12:06:31 2024 +0900
    
        🚧 public ogpImageを移動
    
    commit 6a6235587feeb4c098573c8f97b22efd32d6ded1
    Author: ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
    Date:   Thu Nov 7 11:57:56 2024 +0900
    
        ➖  react-helmet 削除
    
    commit e910bc85400b7ff46ec928899f8fbb3bdb57d8a6
    Author: ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
    Date:   Wed Nov 6 11:34:30 2024 +0900
    
        🚧 title など変更
    
    commit aa6ca71a5c6176bffcd34d27de0ef7f6625bc5d2
    Author: ROTO0504 <88580483+ROTO0504@users.noreply.github.com>
    Date:   Wed Nov 6 11:23:47 2024 +0900
    
        🚧 OGPテスト

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { styled as p } from "panda/jsx";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useInView } from "react-intersection-observer";
 import circle from "@/assets/svg/background/circle.svg";
 import decorationRight from "@/assets/svg/background/decoration-right.svg";
@@ -67,310 +66,126 @@ export const Route = createFileRoute("/")({
 
     return (
       <Expanded alignItems="start">
-        <HelmetProvider>
-          <Helmet
-            meta={[
-              { name: "twitter:card", content: "summary" },
-              { name: "twitter:title", content: "Wakaba" },
-              { name: "twitter:description", content: "HOME | Wakaba" },
-              {
-                name: "twitter:image",
-                content: "/src/assets/img/ogp/ogp_main.png",
-              },
-              { property: "og:title", content: "Wakaba" },
-              { property: "og:type", content: "website" },
-              { property: "og:url", content: "http://wkb.pages.dev" },
-              {
-                property: "og:image",
-                content: "/src/assets/img/ogp/ogp_main.png",
-              },
-              { property: "og:description", content: "HOME | Wakaba" },
-            ]}
-            title="Wakaba"
-          >
-            <title>Wakaba</title>
-          </Helmet>
-          <p.div
-            ref={header}
-            backgroundColor="white"
-            display="grid"
-            fadeIn="10"
-            height={{
-              sm: "600px",
-              lg: "40vh",
-              xl: "50vh",
-            }}
-            placeItems="center"
-            py="20"
-            style={{
-              position: "relative",
-              backgroundImage: `url(${headerRight}), url(${headerLeft})`,
-              backgroundRepeat: "no-repeat, no-repeat",
-              backgroundPosition: "right top, left bottom",
-              backgroundSize: "auto 50%, auto 50%",
-            }}
-          >
-            {inViewHeader && (
-              <LogoComposite
-                duration="1000"
-                fadeIn="10"
-                slideInY="10"
-                style={{
-                  zIndex: 1,
-                  position: "relative",
-                }}
-                zoom={{
-                  base: 2,
-                  smDown: 1.5,
-                }}
-              />
-            )}
-          </p.div>
+        <p.div
+          ref={header}
+          backgroundColor="white"
+          display="grid"
+          fadeIn="10"
+          height={{
+            sm: "600px",
+            lg: "40vh",
+            xl: "50vh",
+          }}
+          placeItems="center"
+          py="20"
+          style={{
+            position: "relative",
+            backgroundImage: `url(${headerRight}), url(${headerLeft})`,
+            backgroundRepeat: "no-repeat, no-repeat",
+            backgroundPosition: "right top, left bottom",
+            backgroundSize: "auto 50%, auto 50%",
+          }}
+        >
+          {inViewHeader && (
+            <LogoComposite
+              duration="1000"
+              fadeIn="10"
+              slideInY="10"
+              style={{
+                zIndex: 1,
+                position: "relative",
+              }}
+              zoom={{
+                base: 2,
+                smDown: 1.5,
+              }}
+            />
+          )}
+        </p.div>
 
-          <p.div
-            ref={ref1}
-            display="grid"
-            fadeIn="2"
-            height={{ base: "400px", sm: "600px", xl: "60vh" }}
-            placeItems="center"
-            style={{
-              position: "relative",
-              backgroundImage: `url(${wakabaIcon}), url(${decorationRight})`,
-              backgroundSize: "auto 15%, auto 15%",
-              backgroundPosition: "5% bottom , 85% bottom",
-              backgroundRepeat: "no-repeat, no-repeat",
-            }}
-          >
-            {inView1 && (
-              <p.div duration="1000" fadeIn="5" slideInY="10">
-                <p.h2 fontSize="3rem" fontWeight="bold" textAlign="center">
-                  Wakabaとは
-                </p.h2>
+        <p.div
+          ref={ref1}
+          display="grid"
+          fadeIn="2"
+          height={{ base: "400px", sm: "600px", xl: "60vh" }}
+          placeItems="center"
+          style={{
+            position: "relative",
+            backgroundImage: `url(${wakabaIcon}), url(${decorationRight})`,
+            backgroundSize: "auto 15%, auto 15%",
+            backgroundPosition: "5% bottom , 85% bottom",
+            backgroundRepeat: "no-repeat, no-repeat",
+          }}
+        >
+          {inView1 && (
+            <p.div duration="1000" fadeIn="5" slideInY="10">
+              <p.h2 fontSize="3rem" fontWeight="bold" textAlign="center">
+                Wakabaとは
+              </p.h2>
 
-                <p.p fontSize="1rem" mt="20px" mx="auto" textAlign="center">
-                  アイデアの種をあつめ形にするサービスです。
-                  <br />
-                  あなたの投稿したアイデアがこの町の明日を豊かにするかも！
-                </p.p>
-              </p.div>
-            )}
-          </p.div>
+              <p.p fontSize="1rem" mt="20px" mx="auto" textAlign="center">
+                アイデアの種をあつめ形にするサービスです。
+                <br />
+                あなたの投稿したアイデアがこの町の明日を豊かにするかも！
+              </p.p>
+            </p.div>
+          )}
+        </p.div>
 
-          <p.div
-            ref={ref4}
-            background="wkb.primary"
-            display="grid"
-            fadeIn="2"
-            height={{ base: "400px", sm: "600px", xl: "60vh" }}
-            placeItems="center"
-            style={{
-              backgroundImage: `url(${wakaba}), url(${circle})`,
-              backgroundSize: "auto 40%, auto 5%",
-              backgroundPosition: "5% bottom , 73% 20%",
-              backgroundRepeat: "no-repeat, no-repeat",
-            }}
-          >
-            {inView4 && (
-              <p.div
-                ref={ref2}
-                display="grid"
-                fadeIn="5"
-                height="100%"
-                placeItems="center"
-                width="100%"
-              >
-                {inView2 && (
-                  <p.div duration="1000" placeItems="center" slideInY="10">
-                    <p.h2
-                      color="wkb.bg"
-                      fadeIn="5"
-                      fontSize="3rem"
-                      fontWeight="bold"
-                      textAlign="center"
-                    >
-                      種を植える
-                    </p.h2>
-
-                    <p.p
-                      color="wkb.bg"
-                      fadeIn="5"
-                      fontSize="1rem"
-                      mt="20px"
-                      mx="auto"
-                      textAlign="center"
-                    >
-                      意見を投稿して、住みやすい街を
-                      <br />
-                      作るアイデアの種を植えよう！
-                    </p.p>
-                    <Link to="/seeds/new">
-                      <Button
-                        _hover={{
-                          background: "wkb.bg",
-                          shadow: "md",
-                        }}
-                        background="wkb.bg"
-                        display="flex"
-                        duration="3000"
-                        fadeIn="5"
-                        fontSize="150%"
-                        fontWeight="bold"
-                        mt="40px"
-                        mx="auto"
-                        slideInY="10"
-                      >
-                        実際に初めてみる
-                      </Button>
-                    </Link>
-                  </p.div>
-                )}
-              </p.div>
-            )}
-          </p.div>
-
-          <p.div
-            ref={ref3}
-            background="wkb.bg"
-            display="grid"
-            height={{ base: "500px", sm: "800px", xl: "60vh" }}
-            placeItems="center"
-            width={{ base: "100%", sm: "100%" }}
-          >
-            {inView3 && (
-              <p.div
-                display="grid"
-                duration="1000"
-                fadeIn="5"
-                placeItems="center"
-                slideInY="10"
-                width="100%"
-              >
-                <p.div width="100%">
+        <p.div
+          ref={ref4}
+          background="wkb.primary"
+          display="grid"
+          fadeIn="2"
+          height={{ base: "400px", sm: "600px", xl: "60vh" }}
+          placeItems="center"
+          style={{
+            backgroundImage: `url(${wakaba}), url(${circle})`,
+            backgroundSize: "auto 40%, auto 5%",
+            backgroundPosition: "5% bottom , 73% 20%",
+            backgroundRepeat: "no-repeat, no-repeat",
+          }}
+        >
+          {inView4 && (
+            <p.div
+              ref={ref2}
+              display="grid"
+              fadeIn="5"
+              height="100%"
+              placeItems="center"
+              width="100%"
+            >
+              {inView2 && (
+                <p.div duration="1000" placeItems="center" slideInY="10">
                   <p.h2
-                    color="wkb.text"
-                    fontSize="300%"
+                    color="wkb.bg"
+                    fadeIn="5"
+                    fontSize="3rem"
                     fontWeight="bold"
                     textAlign="center"
                   >
-                    Projects
+                    種を植える
                   </p.h2>
+
                   <p.p
-                    color="wkb.text"
-                    fontSize="100%"
-                    maxWidth={{ base: "85%", sm: "70%" }}
+                    color="wkb.bg"
+                    fadeIn="5"
+                    fontSize="1rem"
                     mt="20px"
                     mx="auto"
                     textAlign="center"
                   >
-                    試しに好きなジャンルのプロジェクトを調べてみる
+                    意見を投稿して、住みやすい街を
+                    <br />
+                    作るアイデアの種を植えよう！
                   </p.p>
-                </p.div>
-                <p.div
-                  display="grid"
-                  gap="20px"
-                  gridTemplateColumns="repeat(3, 1fr)"
-                  pt={150}
-                  px="5%"
-                  width="100%"
-                >
-                  <p.a height={100} href="/projects?q=cafe" width="100%">
+                  <Link to="/seeds/new">
                     <Button
                       _hover={{
-                        background: "wkb.primary",
+                        background: "wkb.bg",
                         shadow: "md",
                       }}
-                      background="wkb.primary"
-                      color="wkb.bg"
-                      fontSize="150%"
-                      fontWeight="bold"
-                      height="100%"
-                      width="100%"
-                    >
-                      # 休憩
-                    </Button>
-                  </p.a>
-                  <p.a height={100} href="/projects?q=library" width="100%">
-                    <Button
-                      _hover={{
-                        background: "wkb.primary",
-                        shadow: "md",
-                      }}
-                      background="wkb.primary"
-                      color="wkb.bg"
-                      fontSize="150%"
-                      fontWeight="bold"
-                      height="100%"
-                      width="100%"
-                    >
-                      # 施設
-                    </Button>
-                  </p.a>
-                  <p.a height={100} href="/projects?q=gym" width="100%">
-                    <Button
-                      _hover={{
-                        background: "wkb.primary",
-                        shadow: "md",
-                      }}
-                      background="wkb.primary"
-                      color="wkb.bg"
-                      fontSize="150%"
-                      fontWeight="bold"
-                      height="100%"
-                      width="100%"
-                    >
-                      # 飲食
-                    </Button>
-                  </p.a>
-                </p.div>
-              </p.div>
-            )}
-          </p.div>
-          <p.div
-            ref={ref5}
-            backgroundColor="white"
-            display="grid"
-            height={{ base: "500px", sm: "800px", xl: "60vh" }}
-            placeItems="center"
-            width={{ base: "100%", sm: "100%" }}
-          >
-            {inView5 && (
-              <p.div
-                display="grid"
-                duration="1000"
-                fadeIn="5"
-                placeItems="center"
-                slideInY="10"
-                width="100%"
-              >
-                <p.div width="100%">
-                  <p.h2
-                    color="wkb.text"
-                    fontSize="300%"
-                    fontWeight="bold"
-                    textAlign="center"
-                  >
-                    Overview
-                  </p.h2>
-                  <p.p
-                    color="wkb.text"
-                    fontSize="100%"
-                    maxWidth={{ base: "85%", sm: "70%" }}
-                    mt="20px"
-                    mx="auto"
-                    textAlign="center"
-                  >
-                    マップ上から近くのプロジェクトを探してみる
-                  </p.p>
-                </p.div>
-                <p.div display="grid">
-                  <Link to="/overview">
-                    <Button
-                      _hover={{
-                        background: "wkb.primary",
-                        shadow: "md",
-                      }}
-                      background="wkb.primary"
-                      color="wkb.bg"
+                      background="wkb.bg"
                       display="flex"
                       duration="3000"
                       fadeIn="5"
@@ -380,14 +195,174 @@ export const Route = createFileRoute("/")({
                       mx="auto"
                       slideInY="10"
                     >
-                      近くのプロジェクトを探す
+                      実際に初めてみる
                     </Button>
                   </Link>
                 </p.div>
+              )}
+            </p.div>
+          )}
+        </p.div>
+
+        <p.div
+          ref={ref3}
+          background="wkb.bg"
+          display="grid"
+          height={{ base: "500px", sm: "800px", xl: "60vh" }}
+          placeItems="center"
+          width={{ base: "100%", sm: "100%" }}
+        >
+          {inView3 && (
+            <p.div
+              display="grid"
+              duration="1000"
+              fadeIn="5"
+              placeItems="center"
+              slideInY="10"
+              width="100%"
+            >
+              <p.div width="100%">
+                <p.h2
+                  color="wkb.text"
+                  fontSize="300%"
+                  fontWeight="bold"
+                  textAlign="center"
+                >
+                  Projects
+                </p.h2>
+                <p.p
+                  color="wkb.text"
+                  fontSize="100%"
+                  maxWidth={{ base: "85%", sm: "70%" }}
+                  mt="20px"
+                  mx="auto"
+                  textAlign="center"
+                >
+                  試しに好きなジャンルのプロジェクトを調べてみる
+                </p.p>
               </p.div>
-            )}
-          </p.div>
-        </HelmetProvider>
+              <p.div
+                display="grid"
+                gap="20px"
+                gridTemplateColumns="repeat(3, 1fr)"
+                pt={150}
+                px="5%"
+                width="100%"
+              >
+                <p.a height={100} href="/projects?q=cafe" width="100%">
+                  <Button
+                    _hover={{
+                      background: "wkb.primary",
+                      shadow: "md",
+                    }}
+                    background="wkb.primary"
+                    color="wkb.bg"
+                    fontSize="150%"
+                    fontWeight="bold"
+                    height="100%"
+                    width="100%"
+                  >
+                    # 休憩
+                  </Button>
+                </p.a>
+                <p.a height={100} href="/projects?q=library" width="100%">
+                  <Button
+                    _hover={{
+                      background: "wkb.primary",
+                      shadow: "md",
+                    }}
+                    background="wkb.primary"
+                    color="wkb.bg"
+                    fontSize="150%"
+                    fontWeight="bold"
+                    height="100%"
+                    width="100%"
+                  >
+                    # 施設
+                  </Button>
+                </p.a>
+                <p.a height={100} href="/projects?q=gym" width="100%">
+                  <Button
+                    _hover={{
+                      background: "wkb.primary",
+                      shadow: "md",
+                    }}
+                    background="wkb.primary"
+                    color="wkb.bg"
+                    fontSize="150%"
+                    fontWeight="bold"
+                    height="100%"
+                    width="100%"
+                  >
+                    # 飲食
+                  </Button>
+                </p.a>
+              </p.div>
+            </p.div>
+          )}
+        </p.div>
+        <p.div
+          ref={ref5}
+          backgroundColor="white"
+          display="grid"
+          height={{ base: "500px", sm: "800px", xl: "60vh" }}
+          placeItems="center"
+          width={{ base: "100%", sm: "100%" }}
+        >
+          {inView5 && (
+            <p.div
+              display="grid"
+              duration="1000"
+              fadeIn="5"
+              placeItems="center"
+              slideInY="10"
+              width="100%"
+            >
+              <p.div width="100%">
+                <p.h2
+                  color="wkb.text"
+                  fontSize="300%"
+                  fontWeight="bold"
+                  textAlign="center"
+                >
+                  Overview
+                </p.h2>
+                <p.p
+                  color="wkb.text"
+                  fontSize="100%"
+                  maxWidth={{ base: "85%", sm: "70%" }}
+                  mt="20px"
+                  mx="auto"
+                  textAlign="center"
+                >
+                  マップ上から近くのプロジェクトを探してみる
+                </p.p>
+              </p.div>
+              <p.div display="grid">
+                <Link to="/overview">
+                  <Button
+                    _hover={{
+                      background: "wkb.primary",
+                      shadow: "md",
+                    }}
+                    background="wkb.primary"
+                    color="wkb.bg"
+                    display="flex"
+                    duration="3000"
+                    fadeIn="5"
+                    fontSize="150%"
+                    fontWeight="bold"
+                    mt="40px"
+                    mx="auto"
+                    slideInY="10"
+                  >
+                    近くのプロジェクトを探す
+                  </Button>
+                </Link>
+              </p.div>
+            </p.div>
+          )}
+        </p.div>
       </Expanded>
     );
   },
