f0d5514421b01d7ef6de2839333e82eb6699e035
nasubi916 <r916nis@gmail.com>
2024-11-09 22:44:29 +0900

    ok

--- src/lib/classes/user.ts
+++ src/lib/classes/user.ts
@@ -1,6 +1,6 @@
 import { type Session } from "@supabase/supabase-js";
 import { type UseNavigateResult } from "@tanstack/react-router";
-import { ResultAsync } from "neverthrow";
+import { ok, ResultAsync } from "neverthrow";
 import { match } from "ts-pattern";
 import { Sower } from "./sower";
 import { Sponsor } from "./sponsor";
@@ -106,5 +106,34 @@ export class User {
       .mapErr((error) =>
         Table.transformError(config, "fetchOwnComments", error),
       );
+  }
+
+  public fetchKindOf(): TableResult<
+    | {
+        type: "SOWER";
+        data: Sower;
+      }
+    | {
+        type: "SPONSOR";
+        data: Sponsor;
+      }
+    | {
+        type: "UNKNOWN";
+      }
+  > {
+    const data = ResultAsync.combine([
+      Sower.factories.fromUser(this.id).orElse(() => ok(null)),
+      Sponsor.factories.fromUser(this.id).orElse(() => ok(null)),
+    ]);
+
+    return data.andThen(([sower, sponsor]) => {
+      if (sower != null) {
+        return ok({ type: "SOWER", data: sower } as const);
+      }
+      if (sponsor != null) {
+        return ok({ type: "SPONSOR", data: sponsor } as const);
+      }
+      return ok({ type: "UNKNOWN" } as const);
+    });
   }
 }
